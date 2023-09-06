729d7583b37b8957a4692545290a9b739ef9efd6
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-09-06 20:15:07 +0530

    added

--- Concepts/context-api/src/App.js
+++ Concepts/context-api/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import context from './components/Context'
+import NavBar from './components/NavBar';
+import Parent1 from './components/Parent1';
+import Parent2 from './components/Parent2';
 
 function App() {
+
+  const [theme, setTheme] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <context.Provider value={theme}>
+      <button onClick={() => setTheme(!theme)}>Change theme</button>
+      <NavBar />
+      <Parent1 />
+      <Parent2 />
+    </context.Provider>
   );
 }
 
